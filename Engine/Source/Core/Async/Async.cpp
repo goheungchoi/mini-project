c0db039d15f9571cac8c0790a484e9a958ab2fa9
Goheung Choi <choi147157@gmail.com>
2024-12-06 11:57:05 +0900

    - ClangFormat: Template break, pointer alignment right, reference alignment right.

--- Engine/Source/Core/Async/Async.cpp
+++ Engine/Source/Core/Async/Async.cpp
@@ -1,18 +1,21 @@
 #include "Async.h"
 
-struct ThreadPool {
+#include <thread>
 
+struct ThreadPool
+{
 
+  std::vector<std::thread> threads;
 
+  std::vector<async::Task> tasks;
 
+  void RecieveTask(const async::Task& taks)
+  {
+  }
 };
 
-static ThreadPool& GetThreadPool() {
+static ThreadPool& GetThreadPool()
+{
   static ThreadPool _threadPool;
   return _threadPool;
 }
-
-
-
-
-
--- Engine/Source/Core/Async/Async.h
+++ Engine/Source/Core/Async/Async.h
@@ -1,81 +1,101 @@
 #pragma once
 
-#include "Core/Common.h"
-
 #include <atomic>
+
+#include "Core/Common.h"
 
 // TODO: Goheung Choi
 
-namespace async {
+namespace async
+{
 
 template <typename T>
-class Future {
- public:
-
-	 void Wait() {
-
-	 }
-
-	 void WaitFor(uint64_t millisecond) {
-
-	 }
-};
-
-template<typename... Ts>
-class MultiFuture {
-
+class Future
+{
 public:
+  void Wait()
+  {
+  }
 
-	void WaitAny() {}
-
-	void WaitAnyForAll(uint64_t millisecond) {}
-
-  void WaitAll() {}
-
-  void WaitAllFor(uint64_t millisecond) {}
-
+  void WaitFor(uint64_t millisecond)
+  {
+  }
 };
 
-class Task {
+template <typename... Ts>
+class MultiFuture
+{
+public:
+  void WaitAny()
+  {
+  }
+
+  void WaitAnyForAll(uint64_t millisecond)
+  {
+  }
+
+  void WaitAll()
+  {
+  }
+
+  void WaitAllFor(uint64_t millisecond)
+  {
+  }
+};
+
+class Task
+{
   std::atomic<bool> _state;
 
-	std::function<void()> _taskContainer;
+  std::function<void()> _taskContainer;
 
 	std::any _retVal;
- public:
+
+public:
   Task() noexcept;
 
-  Task(Task&& other) noexcept;
-  Task& operator=(Task&& other) noexcept;
+  Task(Task &&other) noexcept;
+  Task &operator=(Task &&other) noexcept;
 
-	bool IsReady() { return _state; }
+  template <typename Ret>
+  Future<Ret> GetFuture()
+  {
+  }
 
-	template<typename Ret, typename... Args>
-	static Task CreateTask(std::function<Ret(Args...)> callable) {
+  bool IsReady()
+  {
+    return _state;
+  }
 
-	}
+  template <typename Ret, typename... Args>
+  static Task CreateTask(std::function<Ret(Args...)> callable)
+  {
+  }
 
-	template<typename Ret, std::invocable Func, typename... Args>
-	static Task CreateTask(Func f, Args... args) {
-		
-	}
+  template <typename Ret, std::invocable Func, typename... Args>
+  static Task CreateTask(Func f, Args... args)
+  {
+  }
 };
 
-class TaskGroup {
- public:
+class TaskGroup
+{
+public:
 };
 
+template <typename Ret, typename... Args>
+Future<Ret> SubmitTask(std::function<Ret(Args...)> &&callable)
+{
+}
 
-
-template<typename Ret, typename... Args>
-Future<Ret> SubmitTask(std::function<Ret(Args...)>&& callable);
-
-template<typename Ret, std::invocable Func, typename... Args>
-Future<Ret> SubmitTask(Func f, Args... args);
+template <typename Ret, std::invocable Func, typename... Args>
+Future<Ret> SubmitTask(Func f, Args... args)
+{
+}
 
 template <std::invocable Func, typename... Args>
 void DispatchTask(Func f, Args... args);
 
 void WaitIdle();
 
-}  // namespace async
+} // namespace async
