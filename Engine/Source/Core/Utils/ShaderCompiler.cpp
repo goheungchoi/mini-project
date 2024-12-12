2c5e166b911ce9061abb228a6a2e4d8ce1c8bbfb
szzelln <127702953+szzelln@users.noreply.github.com>
2024-12-12 11:15:04 +0900

    Fixed bugs

--- Engine/Source/Core/Utils/ShaderCompiler.cpp
+++ Engine/Source/Core/Utils/ShaderCompiler.cpp
@@ -1,9 +1,9 @@
 #include "ShaderCompiler.h"
 
-bool PopulateShaderVariants(size_t numDefineOptions,
-                            const ShaderDefineOption* defineOptions,
-                            std::vector<std::vector<ShaderDefine>>* outVariants)
-{
-	// TODO:
-  return false;
-}
+//bool PopulateShaderVariants(size_t numDefineOptions,
+//                            const ShaderDefineOption* defineOptions,
+//                            std::vector<std::vector<ShaderDefine>>* outVariants)
+//{
+//	// TODO:
+//  return false;
+//}
