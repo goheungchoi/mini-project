a9c7b643d1c1b8dff2ab9562e48736c2829d0b6d
성우 진 <blacknaco85@gmail.com>
2024-12-20 01:13:46 +0900

    add Render Test Proj

---
+++ RendererTest/Application/GameApp/GameApp.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "../../Engine/Source/Engine/GameEngine.h"
+
+class GameApp : public GameEngine
+{
+	using Super = GameEngine;
+
+public:
+  GameApp() = default;
+  ~GameApp() = default;
+
+public:
+  virtual void Initialize() override;
+  virtual void Execute() override;
+  virtual void Shutdown() override;
+
+protected:
+  virtual void FixedUpdate(float deltaTime) override;
+  virtual void Update(float deltaTime) override;
+  virtual void Render() override;
+
+};
