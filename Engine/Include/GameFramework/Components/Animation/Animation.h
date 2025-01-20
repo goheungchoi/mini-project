e54eeb840c21756e0543af4d1d976e10fa06f6ac
goheungchoi <choi147157@gmail.com>
2025-01-20 21:58:01 +0900

    - Animation and Animator.

--- Engine/Include/GameFramework/Components/Animation/Animation.h
+++ Engine/Include/GameFramework/Components/Animation/Animation.h
@@ -2,168 +2,280 @@
 
 #include "ResourceManager/ResourceManager.h"
 
-// TODO:
-class CAnimationChannel
+#include "GameFramework/Components/TransformComponent.h"
+
+class AnimationChannel
 {
-//  const AnimationData* data;
-//
-//	XMMATRIX _localTransform;
-//
-//public:
-//
-//	Animation(const std::string& animationPath) {
-//    auto animationHandle = LoadAnimation(animationPath);
-//    data = &AccessAnimationData(animationHandle);
-//	}
-//
-//		/**
-//   * @brief Interpolates between positions, rotations, and scaling keys
-//   * based on the current time of the animation and prepares the local
-//   * transformation matrix by combining all keys transformations.
-//   * @param animationTime The current animation time
-//   */
-//  void Update(float animationTime)
-//  {
-//    XMMATRIX translate = InterpolatePosition(animationTime);
-//    XMMATRIX rotation = InterpolateRotation(animationTime);
-//    XMMATRIX scaling = InterpolateScaling(animationTime);
-//    _localTransform = scaling * rotation * translate;
-//  }
-//
-//	XMMATRIX GetLocalTransform() const { return _localTransform; }
-//  std::string GetBoneName() const { return _name; }
-//  int GetBoneID() const { data->; }
-//
-//		/**
-//   * @brief Find the current index of the keyframe position to interpolate.
-//   * based on the current animation time.
-//   * @param animationTime The current animation time
-//   * @return The index of the keyframe position.
-//   */
-//  int GetPositionIndex(float animationTime) const
-//  {
-//    for (int index = 0; index < _numPositions - 1; ++index)
-//    {
-//      if (animationTime < _positions[index + 1].timeStamp)
-//        return index;
-//    }
-//    return -1;
-//  }
-//
-//  /**
-//   * @brief Find the current index of the keyframe rotation to interpolate.
-//   * @param animationTime The current animation time
-//   * @return The index of the keyframe rotation.
-//   */
-//  int GetRotationIndex(float animationTime) const
-//  {
-//    for (int index = 0; index < _numRotations - 1; ++index)
-//    {
-//      if (animationTime < _rotations[index + 1].timeStamp)
-//        return index;
-//    }
-//    return -1;
-//  }
-//
-//  /**
-//   * @brief Find the current index of the keyframe scale to interpolate.
-//   * @param animationTime The current animation time
-//   * @return The index of the keyframe scale.
-//   */
-//  int GetScaleIndex(float animationTime) const
-//  {
-//    for (int index = 0; index < _numScalings - 1; ++index)
-//    {
-//      if (animationTime < _scales[index + 1].timeStamp)
-//        return index;
-//    }
-//    return -1;
-//  }
-//
-//private:
-//		/**
-//   * @brief Get the normalized scale factor for lerp and slerp.
-//   * @param prevKeyframeTimeStamp The time stamp of the previous keyframe.
-//   * @param nextKeyframeTimeStamp The time stamp of the next keyframe.
-//   * @param animationTime The current animation time.
-//   * @return The scale factor in [0, 1].
-//   */
-//  float GetScaleFactor(float prevKeyframeTimeStamp, float nextKeyframeTimeStamp,
-//                       float animationTime)
-//  {
-//    float scaleFactor{0.f};
-//    float midwayLength = animationTime - prevKeyframeTimeStamp;
-//    float keyframeLength = nextKeyframeTimeStamp - prevKeyframeTimeStamp;
-//    scaleFactor = midwayLength / keyframeLength;
-//    return scaleFactor;
-//  }
-//
-//	
-//	XMMATRIX InterpolatePosition(float animationTime)
-//  {
-//    if (_numPositions == 1)
-//      return XMMatrixTranslationFromVector(_positions[0].position);
-//
-//    int p0Index = GetPositionIndex(animationTime);
-//    int p1Index = p0Index + 1;
-//
-//    float scaleFactor =
-//        GetScaleFactor(_positions[p0Index].timeStamp,
-//                       _positions[p1Index].timeStamp, animationTime);
-//
-//    XMVECTOR interpolatedPosition =
-//        XMVectorLerp(_positions[p0Index].position, _positions[p1Index].position,
-//                     scaleFactor);
-//
-//    return XMMatrixTranslationFromVector(interpolatedPosition);
-//    // return XMMatrixIdentity();
-//  }
-//
-//  XMMATRIX InterpolateRotation(float animationTime)
-//  {
-//    if (_numRotations == 1)
-//    {
-//      // https://users.ncsa.illinois.edu/kindr/emtc/quaternions/
-//      // For any unit quaternion, q, we have q^(-1) = q^H,
-//      // where q^H = [w, -v], meaning that the inverse is the conjugate.
-//      // Rotation of a 3D vector, v, by a unit quaternion, q, is defined as
-//      // R_q(v) = q[0, v]q^(-1) where [0, v] is a pure quaternion
-//      // built from v by adding a zero real part.
-//      XMVECTOR quatRotation = XMQuaternionNormalize(_rotations[0].orientation);
-//      return XMMatrixRotationQuaternion(quatRotation);
-//    }
-//
-//    int p0Index = GetRotationIndex(animationTime);
-//    int p1Index = p0Index + 1;
-//
-//    float scaleFactor =
-//        GetScaleFactor(_rotations[p0Index].timeStamp,
-//                       _rotations[p1Index].timeStamp, animationTime);
-//
-//    XMVECTOR interpolatedQuatRotation = XMQuaternionSlerp(
-//        XMQuaternionNormalize(_rotations[p0Index].orientation),
-//        XMQuaternionNormalize(_rotations[p1Index].orientation), scaleFactor);
-//    return XMMatrixRotationQuaternion(interpolatedQuatRotation);
-//    // return XMMatrixRotationQuaternion(XMQuaternionIdentity());
-//  }
-//
-//  XMMATRIX InterpolateScaling(float animationTime)
-//  {
-//    if (_numScalings == 1)
-//    {
-//      return XMMatrixScalingFromVector(_scales[0].scale);
-//    }
-//
-//    int p0Index = GetScaleIndex(animationTime);
-//    int p1Index = p0Index + 1;
-//
-//    float scaleFactor = GetScaleFactor(
-//        _scales[p0Index].timeStamp, _scales[p1Index].timeStamp, animationTime);
-//
-//    XMVECTOR interpolatedScale = XMVectorLerp(
-//        _scales[p0Index].scale, _scales[p1Index].scale, scaleFactor);
-//    return XMMatrixScalingFromVector(interpolatedScale);
-//  }
-//
+  const AnimationChannelData* data;
+
+public:
+  XMMATRIX localTransform{};
+
+  AnimationChannel(const AnimationChannelData* data)
+      : data{data}
+  {}
+
+  const XMMATRIX& GetLocalTransform() const { return localTransform; }
+
+  const std::string& GetNodeName() const { return data->nodeName; }
+
+  int GetBoneID() const { return data->boneId; }
+
+  uint32_t GetNumKeyPositions() const { return data->numKeyPositions; }
+  uint32_t GetNumKeyRotations() const { return data->numKeyRotations; }
+  uint32_t GetNumKeyScalings() const { return data->numKeyScalings; }
+
+  const std::vector<KeyPosition>& GetKeyPositions() const
+  {
+    return data->keyPositions;
+  }
+  const std::vector<KeyRotation>& GetKeyRotations() const
+  {
+    return data->keyRotations;
+  }
+  const std::vector<KeyScaling>& GetKeyScalings() const
+  {
+    return data->keyScalings;
+  };
+
+  void Update(float animationTime) {
+    XMMATRIX translate = InterpolatePosition(animationTime);
+    XMMATRIX rotation = InterpolateRotation(animationTime);
+    XMMATRIX scaling = InterpolateScaling(animationTime);
+    localTransform = scaling * rotation * translate;
+  }
+
+	/**
+   * @brief Find the current index of the keyframe position to interpolate.
+   * based on the current animation time.
+   * @param animationTime The current animation time
+   * @return The index of the keyframe position.
+   */
+  int GetPositionIndex(float animationTime) const
+  {
+    for (int index = 0; index < GetNumKeyPositions() - 1; ++index)
+    {
+      if (animationTime < GetKeyPositions()[index + 1].timeStamp)
+        return index;
+    }
+    return -1;
+  }
+
+  /**
+   * @brief Find the current index of the keyframe rotation to interpolate.
+   * @param animationTime The current animation time
+   * @return The index of the keyframe rotation.
+   */
+  int GetRotationIndex(float animationTime) const
+  {
+    for (int index = 0; index < GetNumKeyRotations() - 1; ++index)
+    {
+      if (animationTime < GetKeyRotations()[index + 1].timeStamp)
+        return index;
+    }
+    return -1;
+  }
+
+  /**
+   * @brief Find the current index of the keyframe scale to interpolate.
+   * @param animationTime The current animation time
+   * @return The index of the keyframe scale.
+   */
+  int GetScaleIndex(float animationTime) const
+  {
+    for (int index = 0; index < GetNumKeyScalings() - 1; ++index)
+    {
+      if (animationTime < GetKeyScalings()[index + 1].timeStamp)
+        return index;
+    }
+    return -1;
+  }
+
+private:
+  /**
+   * @brief Get the normalized scale factor for lerp and slerp.
+   * @param prevKeyframeTimeStamp The time stamp of the previous keyframe.
+   * @param nextKeyframeTimeStamp The time stamp of the next keyframe.
+   * @param animationTime The current animation time.
+   * @return The scale factor in [0, 1].
+   */
+  float GetScaleFactor(float prevKeyframeTimeStamp, float nextKeyframeTimeStamp,
+                       float animationTime)
+  {
+    float scaleFactor{0.f};
+    float midwayLength = animationTime - prevKeyframeTimeStamp;
+    float keyframeLength = nextKeyframeTimeStamp - prevKeyframeTimeStamp;
+    scaleFactor = midwayLength / keyframeLength;
+    return scaleFactor;
+  }
+
+  XMMATRIX InterpolatePosition(float animationTime)
+  {
+    if (GetNumKeyPositions() == 1)
+      return XMMatrixTranslationFromVector(GetKeyPositions()[0].position);
+
+    int p0Index = GetPositionIndex(animationTime);
+    int p1Index = p0Index + 1;
+
+    float scaleFactor =
+        GetScaleFactor(GetKeyPositions()[p0Index].timeStamp,
+                       GetKeyPositions()[p1Index].timeStamp, animationTime);
+
+    XMVECTOR interpolatedPosition =
+        XMVectorLerp(GetKeyPositions()[p0Index].position,
+                     GetKeyPositions()[p1Index].position,
+                     scaleFactor);
+
+    return XMMatrixTranslationFromVector(interpolatedPosition);
+    // return XMMatrixIdentity();
+  }
+
+  XMMATRIX InterpolateRotation(float animationTime)
+  {
+    if (GetNumKeyRotations() == 1)
+    {
+      // https://users.ncsa.illinois.edu/kindr/emtc/quaternions/
+      // For any unit quaternion, q, we have q^(-1) = q^H,
+      // where q^H = [w, -v], meaning that the inverse is the conjugate.
+      // Rotation of a 3D vector, v, by a unit quaternion, q, is defined as
+      // R_q(v) = q[0, v]q^(-1) where [0, v] is a pure quaternion
+      // built from v by adding a zero real part.
+      XMVECTOR quatRotation =
+          XMQuaternionNormalize(GetKeyRotations()[0].orientation);
+      return XMMatrixRotationQuaternion(quatRotation);
+    }
+
+    int p0Index = GetRotationIndex(animationTime);
+    int p1Index = p0Index + 1;
+
+    float scaleFactor =
+        GetScaleFactor(GetKeyRotations()[p0Index].timeStamp,
+                       GetKeyRotations()[p1Index].timeStamp, animationTime);
+
+    XMVECTOR interpolatedQuatRotation = XMQuaternionSlerp(
+        XMQuaternionNormalize(GetKeyRotations()[p0Index].orientation),
+        XMQuaternionNormalize(GetKeyRotations()[p1Index].orientation),
+        scaleFactor);
+    return XMMatrixRotationQuaternion(interpolatedQuatRotation);
+    // return XMMatrixRotationQuaternion(XMQuaternionIdentity());
+  }
+
+  XMMATRIX InterpolateScaling(float animationTime)
+  {
+    if (GetNumKeyScalings() == 1)
+    {
+      return XMMatrixScalingFromVector(GetKeyScalings()[0].scaling);
+    }
+
+    int p0Index = GetScaleIndex(animationTime);
+    int p1Index = p0Index + 1;
+
+    float scaleFactor = GetScaleFactor(
+        GetKeyScalings()[p0Index].timeStamp, GetKeyScalings()[p1Index].timeStamp, animationTime);
+
+    XMVECTOR interpolatedScale = XMVectorLerp(GetKeyScalings()[p0Index].scaling,
+                     GetKeyScalings()[p1Index].scaling, scaleFactor);
+    return XMMatrixScalingFromVector(interpolatedScale);
+  }
+};
+
+// TODO:
+class Animation
+{
+  const AnimationData* data{nullptr};
+
+  std::vector<AnimationChannel> channels;
+  std::unordered_map<BoneId, uint32_t> boneIndexMap;
+  std::unordered_map<std::string, uint32_t> nodeIndexMap;
+
+  float _currentAnimTime{0.f}; // Current animation time
+
+  bool isTriggered{false};
+  bool isPlaying{false};
+
+public:
+  bool isLoop;
+
+  Animation(AnimationHandle handle, bool isLoop) : isLoop{isLoop}
+  {
+    data = &AccessAnimationData(handle);
+
+    channels.reserve(data->animationChannels.size());
+    boneIndexMap.reserve(data->animationChannels.size());
+    nodeIndexMap.reserve(data->animationChannels.size());
+
+    uint32_t index = 0;
+    for (const auto& channel : data->animationChannels)
+    {
+      boneIndexMap[channel.boneId] = index;
+      nodeIndexMap[channel.nodeName] = index;
+      channels.emplace_back(&channel);
+    }
+  }
+
+  const std::vector<AnimationChannel> GetChannels() const { return channels; }
+
+  const AnimationChannel* FindChannel(const std::string& nodeName) const
+  {
+    if (auto it = nodeIndexMap.find(nodeName); it != nodeIndexMap.end())
+    {
+      return &channels[it->second];
+    }
+    else
+    {
+      return nullptr;
+    }
+  }
+
+  /**
+   * @brief Get the animation speed
+   * @return ticks per second
+   */
+  float GetTicksPerSecond() { return data->ticksPerSecond; }
+  float GetDuration() { return data->duration; }
+
+
+  bool IsLoop() { return isLoop; }
+  void SetLoop(float isLoop) { this->isLoop = isLoop; }
+
+  void Trigger()
+  {
+    isTriggered = true;
+    isPlaying = true;
+
+    _currentAnimTime = 0.f;
+  }
+
+  void Pause() { isPlaying = false; }
+
+  bool IsPlaying() { return isPlaying; }
+
+  void Quit()
+  {
+    isTriggered = false;
+    isPlaying = false;
+    _currentAnimTime = 0.f;
+  }
+
+  void Update(float dt) 
+  {
+
+
+  }
+
+  void UpdateBoneTransforms(float dt, TransformComponent* parent, const SkeletonData* skeleton,
+                            std::vector<XMMATRIX>& finalBoneTransforms)
+  {
+    _currentAnimTime += GetTicksPerSecond() * dt;
+    _currentAnimTime = fmod(_currentAnimTime, GetDuration());
+
+    for (auto& channel : channels)
+    {
+      channel.Update(_currentAnimTime);
+    }
+
+
+
+  }
 
 };
