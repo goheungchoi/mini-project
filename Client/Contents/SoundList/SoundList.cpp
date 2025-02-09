5cf927a1b06e985d4d012a25d26ce456ea1f982e
goheungchoi <choi147157@gmail.com>
2025-02-09 17:55:29 +0900

    - Sound list

--- Client/Contents/SoundList/SoundList.cpp
+++ Client/Contents/SoundList/SoundList.cpp
@@ -6,10 +6,12 @@ const wchar_t* SoundList::Gunman_Ready3 = L"";
 const wchar_t* SoundList::Gunman_Fire = L"";
 
 const wchar_t* SoundList::Enemy_Die[5] = {
-    L"",
-};
+    L"Enemy_HitSound_01.wav", L"Enemy_HitSound_02.wav",
+    L"Enemy_HitSound_03.wav", L"Enemy_HitSound_04.wav",
+    L"Enemy_HitSound_05.wav"};
 
-const wchar_t* SoundList::Ally_Gunman_Die = L"";
+const wchar_t* SoundList::Ally_Gunman_Die[2] = {L"Gun_HitSound_01.wav",
+                                                L"Gun_HitSound_02.wav"};
 
 
 
