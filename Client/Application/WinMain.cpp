4a64979d7524324326a757b34e24a1366bc29c2c
goheungchoi <choi147157@gmail.com>
2025-02-10 18:16:17 +0900

    - Don't count the assassination target when characters searching for a target.
    - Generate dump file.
    
    - Updated resource files.

--- Client/Application/WinMain.cpp
+++ Client/Application/WinMain.cpp
@@ -12,6 +12,40 @@
 #define new new (_NORMAL_BLOCK, __FILE__, __LINE__)
 #endif
 
+#include <Dbghelp.h>
+#include <tchar.h>
+
+typedef BOOL(WINAPI* MINIDUMPWRITEDUMP)(
+    HANDLE hProcess, DWORD dwPid, HANDLE hFile, MINIDUMP_TYPE DumpType,
+    CONST PMINIDUMP_EXCEPTION_INFORMATION ExceptionParam,
+    CONST PMINIDUMP_USER_STREAM_INFORMATION UserStreamParam,
+    CONST PMINIDUMP_CALLBACK_INFORMATION CallbackParam);
+
+void create_minidump(struct _EXCEPTION_POINTERS* apExceptionInfo)
+{
+  HMODULE mhLib = ::LoadLibrary(_T("dbghelp.dll"));
+  MINIDUMPWRITEDUMP pDump =
+      (MINIDUMPWRITEDUMP)::GetProcAddress(mhLib, "MiniDumpWriteDump");
+
+  HANDLE hFile = ::CreateFile(_T("minidump.dmp"), GENERIC_WRITE, FILE_SHARE_WRITE,
+                              NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+  _MINIDUMP_EXCEPTION_INFORMATION ExInfo;
+  ExInfo.ThreadId = ::GetCurrentThreadId();
+  ExInfo.ExceptionPointers = apExceptionInfo;
+  ExInfo.ClientPointers = FALSE;
+
+  pDump(GetCurrentProcess(), GetCurrentProcessId(), hFile, MiniDumpNormal,
+        &ExInfo, NULL, NULL);
+  ::CloseHandle(hFile);
+}
+
+LONG WINAPI unhandled_exception_handler(struct _EXCEPTION_POINTERS* apExceptionInfo)
+{
+  create_minidump(apExceptionInfo);
+  return EXCEPTION_CONTINUE_SEARCH;
+}
+
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
                     _In_ PWSTR pCmdLine, _In_ int nCmdShow)
 {
@@ -19,6 +53,10 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 #ifdef _DEBUG
   _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
+
+  // Write a dump file when an unhandled exception occurred.
+  SetUnhandledExceptionFilter(unhandled_exception_handler);
+
   GameApp app;
   app.Initialize(kScreenWidth, kScreenHeight, L"Rebellion");
   app.Execute();
