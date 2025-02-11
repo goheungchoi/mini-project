32da5e7ac57b16f6e29292e627d2f087e1f5d7dd
JJ2nu <jinwoo0527@gmail.com>
2025-02-11 19:42:04 +0900

    working on replay logic

--- Client/Contents/UI/InGameUI/PlayButton/PlayButton.cpp
+++ Client/Contents/UI/InGameUI/PlayButton/PlayButton.cpp
@@ -21,27 +21,29 @@ PlayButton::PlayButton(World* world) : UIPanel(world)
 
     _playBtnImgs[0]->SetSprite("2D\\UI\\UI_Play_Act.png", _pos);
     _playBtnImgs[1]->SetSprite("2D\\UI\\UI_Play_Hover.png", _pos);
-    _playBtnImgs[1]->SetStatus(EStatus::EStatus_Inactive);
     _playBtnImgs[2]->SetSprite("2D\\UI\\UI_Play_Deact.png", _pos);
-    _playBtnImgs[2]->SetStatus(EStatus::EStatus_Inactive);
+    _playBtnImgs[0]->SetOpacity(1.f);
+    _playBtnImgs[1]->SetOpacity(0.f);
+    _playBtnImgs[2]->SetOpacity(0.f);
 
     _playBtn = CreateUI<UIButton>(L"PlayBtn");
     _playBtn->SetSize(_playBtnImgs[0]->GetSize());
+    _playBtn->SetDebugDraw(true);
     _playBtn->SetCenterPos(_pos);
 #ifdef _DEBUG
     _playBtn->SetDebugDraw(true);
 #endif // _DEBUG
 
     _playBtn->AddOnHoveredHandler([this]() {
-      _playBtnImgs[0]->SetStatus(EStatus::EStatus_Inactive);
-      _playBtnImgs[1]->SetStatus(EStatus::EStatus_Active);
-      _playBtnImgs[2]->SetStatus(EStatus::EStatus_Inactive);
+      _playBtnImgs[0]->SetOpacity(0.f);
+      _playBtnImgs[1]->SetOpacity(1.f);
+      _playBtnImgs[2]->SetOpacity(0.f);
     });
 
     _playBtn->AddOnUnHoveredHandler([this]() {
-      _playBtnImgs[0]->SetStatus(EStatus::EStatus_Active);
-      _playBtnImgs[1]->SetStatus(EStatus::EStatus_Inactive);
-      _playBtnImgs[2]->SetStatus(EStatus::EStatus_Inactive);
+      _playBtnImgs[0]->SetOpacity(1.f);
+      _playBtnImgs[1]->SetOpacity(0.f);
+      _playBtnImgs[2]->SetOpacity(0.f);
     });
 
     _playBtn->AddOnClickHandler([this]() {
@@ -78,6 +80,13 @@ PlayButton::PlayButton(World* world) : UIPanel(world)
                                ->_gunfireBtn->Deactivate();
       }
     });
+
+  SetOnActivatedEvent([=]() {
+      _playBtn->Activate();
+      _playBtnImgs[0]->SetOpacity(1.f);
+      _playBtnImgs[1]->SetOpacity(0.f);
+      _playBtnImgs[2]->SetOpacity(0.f);
+    });
 }
 
 PlayButton::~PlayButton() {}
@@ -89,9 +98,9 @@ void PlayButton::Update(float dt)
   if (_bPlayflag == true || _map->isActionTriggered)
   {
     _playBtn->Deactivate();
-    _playBtnImgs[0]->SetStatus(EStatus::EStatus_Inactive);
-    _playBtnImgs[1]->SetStatus(EStatus::EStatus_Inactive);
-    _playBtnImgs[2]->SetStatus(EStatus::EStatus_Active);
+    _playBtnImgs[0]->SetOpacity(0.f);
+    _playBtnImgs[1]->SetOpacity(0.f);
+    _playBtnImgs[2]->SetOpacity(1.f);
   }
 
   //if (_world->_canvas->GetPanel<ResultDialogUI>(L"ResultDialogUI")
