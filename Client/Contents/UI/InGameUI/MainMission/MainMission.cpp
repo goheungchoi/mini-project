5454fc2db6879b3e054479dadc3847358f3845a7
cherry980927@gmail.com <cherry980927@gmail.com>
2025-02-11 17:00:45 +0900

    modified mainMission

--- Client/Contents/UI/InGameUI/MainMission/MainMission.cpp
+++ Client/Contents/UI/InGameUI/MainMission/MainMission.cpp
@@ -12,7 +12,7 @@ MainMission::~MainMission() {}
 
 void MainMission::SetText(std::wstring text)
 {
-  Vector2 textPos = {_pos.x-20, _pos.y + 27};
+  Vector2 textPos = {_pos.x-20, _pos.y + 12};
 
   _mainMissionTxt = CreateUI<UIText>(L"MainMissonTxt");
   _mainMissionTxt->SetCenterPos({textPos});
@@ -20,7 +20,7 @@ void MainMission::SetText(std::wstring text)
       {_mainMissionImg->GetSize().x, _mainMissionImg->GetSize().y});
   _mainMissionTxt->SetTextAlignment(TextAlignment::LEFTAlIGN);
   _mainMissionTxt->SetFont(L"PT Noeul");
-  _mainMissionTxt->SetFontSize(40.0f);
+  _mainMissionTxt->SetFontSize(37.0f);
   _mainMissionTxt->SetFontWeight(FontWeight::NORMAL);
   _mainMissionTxt->SetText(text.c_str());
   _mainMissionTxt->SetOpacity(1.0f);
@@ -29,18 +29,17 @@ void MainMission::SetText(std::wstring text)
 #endif // _DEBUG
 
 
-//    _selectTxt = CreateUI<UIText>(L"MainSelectTxt");
-//  _selectTxt->SetCenterPos({textPos.x - 85, textPos.y});
-//  _selectTxt->SetSize(
-//      {_mainMissionImg->GetSize().x, _mainMissionImg->GetSize().y});
-//  _selectTxt->SetTextAlignment(TextAlignment::LEFTAlIGN);
-//  _selectTxt->SetFont(L"PT Noeul");
-//  _selectTxt->SetFontSize(30.0f);
-//  _selectTxt->SetFontWeight(FontWeight::NORMAL);
-//  _selectTxt->SetColor({0.302, 0.024, 0.08, 1});
-//  _selectTxt->SetText(L"(선택)");
-//  _selectTxt->SetOpacity(1.0f);
-//#ifdef _DEBUG
-//  _selectTxt->SetDebugDraw(false);
-//#endif // _DEBUG
+  _defaultTxt = CreateUI<UIText>(L"MainSelectTxt");
+  _defaultTxt->SetCenterPos({textPos.x, textPos.y + 40});
+  _defaultTxt->SetSize(
+      {_mainMissionImg->GetSize().x, _mainMissionImg->GetSize().y});
+  _defaultTxt->SetTextAlignment(TextAlignment::LEFTAlIGN);
+  _defaultTxt->SetFont(L"PT Noeul");
+  _defaultTxt->SetFontSize(30.0f);
+  _defaultTxt->SetFontWeight(FontWeight::NORMAL);
+  _defaultTxt->SetText(L"모든 적 처치");
+  _defaultTxt->SetOpacity(1.0f);
+#ifdef _DEBUG
+  _defaultTxt->SetDebugDraw(false);
+#endif // _DEBUG
 }
