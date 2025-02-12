db2d8edbfa9a1191a6acedbcb02844fee78192a9
SeongU <blacknaco85@gmail.com>
2025-02-12 14:28:31 +0900

    -temp

--- Client/Contents/Levels/ChallengeMode/ChallengeMode.cpp
+++ Client/Contents/Levels/ChallengeMode/ChallengeMode.cpp
@@ -5,17 +5,25 @@
 
 #include "GameFramework/UI/Canvas/Canvas.h"
 #include "GameFramework/UI/UIPanel/UIPanel.h"
-
+#include "Contents/UI/TransitionUI/TransitionUI.h"
 #include "GameFramework/UI/UIImage/UIImage.h"
 
 void ChallengeMode::PrepareLevel()
 {
-
+#ifdef USED2D
+  // Resource2DManager::GetInstance()->LoadSprite("Textures\\TitleLogo.png");
+  Resource2DManager::GetInstance()->LoadSprite(
+      "2D\\UI\\Title\\Challenge_BackGround.png");
+#endif // USED2D
 }
 
 void ChallengeMode::BeginLevel()
 {
-
+#ifdef USED2D
+  transitionUI = world->_canvas->CreatePanel<TransitionUI>(L"FadeTransition");
+  transitionUI->_blackImage->SetOpacity(1.0f);
+  transitionUI->FadeIn(2.f);
+#endif
 }
 
 void ChallengeMode::CleanupLevel() {}
